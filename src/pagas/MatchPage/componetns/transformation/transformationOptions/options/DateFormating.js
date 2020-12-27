87a32ac99bcc753373a2b97b277722decbfd91b5
Hen Shalom <xdrakolax@gmail.com>
2020-12-28 01:48:26 +0200

    add Date formating

--- src/pagas/MatchPage/componetns/transformation/transformationOptions/options/DateFormating.js
+++ src/pagas/MatchPage/componetns/transformation/transformationOptions/options/DateFormating.js
@@ -1,35 +1,42 @@
 import React from 'react'
-import { Box, Flex } from 'rebass'
-import { Label, Input } from '@rebass/forms'
+import { Input, Space } from 'antd';
+
 
 import './DatFormatting.css'
 
-function DateFormating() {
-  return (<div className="date-format-picker">
-    <Flex pl="2vw"
-      pt="2vh"
-      flexDirection="column"
-      sx={{ width: "80%" }}>
-      <Box mt="4vh">
-        <Label htmlFor='date-in'>Date Format In</Label>
-        <Input
-          id='date-in'
-          name='email'
-          type='email'
-          placeholder='jane@example.com'
-        />
-      </Box>
+const inputStyle = {
+  width: "30vw",
+  height: "6vh",
+  margin: "4vh  0 0 4vw",
+  borderRadius: "2vh",
+  padding: "0vh 1vw",
+  outline: "none",
+  fontSize: "2vh"
+}
 
-      <Box mt="4vh">
-        <Label htmlFor='date-out'>Date Fromat Out</Label>
-        <Input
-          id='date-out'
-          name='email'
-          type='email'
-          placeholder='jane@example.com'
-        />
-      </Box>
-    </Flex>
+function DateFormating({ onChange, settings }) {
+  return (<div className="date-format-picker">
+    <Space direction="vertical">
+      <Input
+        size="large"
+        onChange={({ target }) => { onChange({ in: target.value }) }}
+        id='date-in'
+        name='date format in'
+        placeholder='YYYY-MM-DD HH:mm'
+        style={inputStyle}
+      />
+      <Input
+        size="large"
+        onChange={({ target }) => { onChange({ out: target.value }) }}
+        value={settings.out}
+        id='date-out'
+        name='date format out'
+        placeholder='YYYY-MM-DD'
+        style={inputStyle}
+
+      />
+    </Space>
+
   </div >)
 }
 
